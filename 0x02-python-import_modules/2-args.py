5176c14e8218d8beab4ed1e6f60f41ed39c7b498
amer2040 <aboodamer7@gmail.com>
2023-09-07 04:44:41 -0700

    update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    c = len(sys.argv) - 1
+    if c == 0:
+        print('0 arguments.')
+    elif c == 1:
+        print('1 argument:')
+    else:
+        print('{} arguments:'.format(c))
+    for x in range(c):
+        print('{}: {}'.format(x + 1, sys.argv[x+1]))
