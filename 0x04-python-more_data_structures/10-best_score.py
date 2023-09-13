2b8e61279522d37dfe2e0a4b32031bd9a3006068
amer2040 <aboodamer7@gmail.com>
2023-09-13 10:07:41 -0700

    update

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        big = 0
+        key = None
+        for k, v in a_dictionary.items():
+            if v > big:
+                big = v
+                key = k
+        return key
+                
