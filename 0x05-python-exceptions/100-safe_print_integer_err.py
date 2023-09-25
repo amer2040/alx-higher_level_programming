b2117402d25e7e3b3e68e59d305e1b7b545b5ee8
amer2040 <aboodamer7@gmail.com>
2023-09-25 08:46:46 -0700

    update

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    num = True
+    try:
+        print('{:d}'.format(value))
+    except Exception as e:
+        print('Exception:', e, file=sys.stderr)
+        num = False
+    return num
