d477b72ef51c20ff1ec9d8d198e988b1d0d5e974
amer2040 <aboodamer7@gmail.com>
2023-10-02 10:34:03 -0700

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,10 +17,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set for the private instance attr width"""
-        if value < 0:
-            raise ValueError("width must be >= 0")
         if type(value) is not int:
             raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -31,8 +31,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set for the private instance attr height"""
-        if value < 0:
-            raise ValueError("height must be >= 0")
         if type(value) is not int:
             raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
