e4b41f6167fbe4e6050d412903411cc6b7b13aa4
amer2040 <aboodamer7@gmail.com>
2023-10-03 09:03:41 -0700

    update

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a locked class"""
+
+
+class LockedClass:
+    """
+    Locked Class  just print if get 'first_name'
+    """
+
+    __slots__ = ["first_name"]
---
+++ 0x09-python-everything_is_object/u.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import os
+os.system('git add . && git commit -m "update" && git push')
