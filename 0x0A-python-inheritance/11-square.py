8a5563f29311ca68697e57b69aba7bc76258706a
amer2040 <aboodamer7@gmail.com>
2023-10-09 06:57:43 -0700

    update

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''module for Square class'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Square subclass'''
+    def __init__(self, size):
+        '''constructor'''
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''area method of square'''
+        return self.__size ** 2
+
+    def __str__(self):
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
