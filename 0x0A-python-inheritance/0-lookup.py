65a1ebd8a37c2009406b6f38cda3abfbd36e5da1
amer2040 <aboodamer7@gmail.com>
2023-10-09 04:45:53 -0700

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""Nodule for lookup method."""
+
+
 def lookup(obj):
+    """check object given
+    Args:
+        obj: is object given to list
+
+    Return:
+        list: the list of attributes.
+    """
     return dir(obj)
