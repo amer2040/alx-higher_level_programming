9dcaa960dbfb37f496cdf2cf457987854bf49a79
amer2040 <aboodamer7@gmail.com>
2023-10-09 07:12:01 -0700

    update

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''module for addnewattr method'''
+
+
+def addnewattr(obj, attr, value):
+    '''Add new attribute to an object if found.
+    Args:
+        obj: the object to add an attribute.
+        attr: the attribute name.
+        value: the value of attr.
+    Raise:
+        TypeError: If cannot add the attribute
+    '''
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
