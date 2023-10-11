dea7bbf61d21e2e1477168415a291a4620dead08
amer2040 <aboodamer7@gmail.com>
2023-10-11 07:24:32 -0700

    update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''module for write_file method'''
+
+
+def write_file(filename="", text=""):
+    '''write string to text file and returns number of chars written'''
+    with open(filename, 'w', encoding='UTF8') as file:
+        txt = file.write(text)
+
+    return len(txt)
