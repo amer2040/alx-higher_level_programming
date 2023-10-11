492635bc769db373aaf220ed252f041f6d5d5d29
amer2040 <aboodamer7@gmail.com>
2023-10-11 07:15:22 -0700

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''module for read_file method'''
+
+
+def read_file(filename=""):
+    '''read_file method'''
+    with open(filename, 'r', encoding='UTF8') as file:
+        file_read = file.read()
+
+    print(file_read)
