706773acc25ee89cf01d087ccd6984b1bb013321
amer2040 <aboodamer7@gmail.com>
2023-10-14 19:21:38 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''module for Base class'''
+from json import loads, dumps
+import csv
 
 
 class Base:
@@ -14,3 +16,82 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''convert list of dictionaries to json string'''
+        if list_dictionaries is None or not list_dictionaries:
+            return '[]'
+        else:
+            return dumps(list_dictionaries)
+    
+    @staticmethod
+    def from_json_string(json_string):
+        '''convert json string to list of dictionaries'''
+        if json_string is None or not json_string:
+            return []
+        else:
+            return loads(json_string)
+    
+    @classmethod
+    def save_to_files(cls, list_objs):
+        '''saves jason object to files'''
+        if list_objs is not None:
+            list_objs = [x.to_dictionary() for x in list_objs]
+        with open(f'{cls.__name__}.json', 'w', encoding='utf-8') as file:
+            file.write(cls.to_json_string(list_objs))
+    
+    @classmethod
+    def load_from_files(cls):
+        '''loads json object from files'''
+        from os import path
+        f = f'{cls.__name__}.json'
+        if path.isfile(f):
+            return []
+        with open(f, 'r', encoding='utf-8') as file:
+            return [cls.create(**x) for x in cls.from_json_string(file.read())]
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''loads instance from dictionary'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            new = Rectangle(1, 1)
+        elif cls is Square:
+            new = Square(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new
+    
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''save object to csv file'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                list_objs = [[x.id, x.width, x.height, x.x, x.y] for x in list_objs]
+            else:
+                list_objs = [[x.id, x.size, x.x, x.y] for x in list_objs]
+        with open(f'{cls.__name__}.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(list_objs)
+    
+    @classmethod
+    def load_from_file_csv(cls):
+        '''loads object from csv file'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        i = []
+        with open(f'{cls.__name__}.csv', 'r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                row = [int(r) for r in row]
+                if cls is Rectangle:
+                    x = {'id': row[0], 'width': row[1], 'height': row[2], 'x': row[3], 'y': row[4]}
+                else:
+                    x = {'id': row[0],'size': row[1], 'x': row[2], 'y': row[3]}
+                i.append(cls.create(**x))
+        return i
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+'''module for Rectangle class'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''Rectangle class'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''constructor'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
+    @property
+    def width(self):
+        '''get width'''
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        '''set width'''
+        self.validate_integer('width', value, False)
+        self.__width = value
+    
+    @property
+    def height(self):
+        '''get height'''
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        '''set height'''
+        self.validate_integer('height', value, False)
+        self.__height = value
+    
+    @property
+    def x(self):
+        '''get x'''
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        '''set x'''
+        self.validate_integer('x', value)
+        self.__x= value
+    
+    @property
+    def y(self):
+        '''get y'''
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        '''set y'''
+        self.validate_integer('y', value)
+        self.__y = value
+    
+    def validate_integer(self, name, value, eq=True):
+        '''validate the value'''
+        if type(value) != int:
+            raise TypeError(f'{name} must be an integer')
+        elif eq and value < 0:
+            raise ValueError(f'{name} must be >= 0')
+        elif value <= 0 and not eq:
+            raise ValueError(f'{name} must be > 0')
+    
+    def area(self):
+        '''get area of the rectangle'''
+        return self.width * self.height
+    
+    def display(self):
+        '''print representation of the rectangle in string'''
+        string = '\n' * self.y + (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(string, end='')
+
+    def __str__(self):
+        '''Returns info about the rectangle in string'''
+        return f'[{type(self).__name__}] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}'
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''private method to updates instance attributes by */**args'''
+        if id is not None:
+            self.id = id
+        elif width is not None:
+            self.width = width
+        elif height is not None:
+            self.height = height
+        elif x is not None:
+            self.x = x
+        elif y is not None:
+            self.y = y
+    
+    def update(self, *args, **kwargs):
+        '''update instance attributes by keyword args and no-keyword'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+    
+    def to_dictionary(self):
+        '''return dictionary representation of the rectangle'''
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
+        }
