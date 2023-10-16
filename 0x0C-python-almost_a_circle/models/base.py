dcbf3fea6cb0461ae9f456ca487350c8cef275f6
amer2040 <aboodamer7@gmail.com>
2023-10-16 19:33:29 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -94,8 +94,8 @@ class Base:
             for row in reader:
                 row = [int(r) for r in row]
                 if cls is Rectangle:
-                    x = {'id': row[0], 'width': row[1], 'height': row[2],
-                        'x': row[3], 'y': row[4]}
+                    x = {'id': row[0], 'width': row[1],
+                         'height': row[2], 'x': row[3], 'y': row[4]}
                 else:
                     x = {'id': row[0], 'size': row[1],
                          'x': row[2], 'y': row[3]}
