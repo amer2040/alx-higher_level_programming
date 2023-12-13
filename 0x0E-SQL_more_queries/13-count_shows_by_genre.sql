1dec487c43332682dfedc13d6eafbffd5d571b61
amer2040 <aboodamer7@gmail.com>
2023-12-13 15:15:32 -0800

    update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,8 +6,8 @@
 -- Results must be sorted in descending order by the number of shows linked
 -- You can use only one SELECT statement
 SELECT tv_genres.name AS genre,
-COUNT(tv_show_genres.genere_id) AS number_of_shows
-FROM tv_how_enres
+COUNT(*) AS number_of_shows
+FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
