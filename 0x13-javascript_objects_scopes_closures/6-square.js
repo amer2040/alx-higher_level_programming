e71caf575e725eb7a9906c408b99cbad4bbb8f40
amer2040 <aboodamer7@gmail.com>
2024-01-09 17:02:25 -0800

    update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const IS = require('./5-square');
+
+class Square extends IS {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+}
+
+module.exports = Square;
