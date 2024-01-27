df38301c25a96d91be9a0afaf63ca81241f0cc74
amer2040 <aboodamer7@gmail.com>
2024-01-27 02:32:00 +0200

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,4 +13,3 @@ if __name__ == '__main__':
         print('\t- type: {}'.format(type(the_page)))
         print('\t- content: {}'.format(the_page))
         print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
-        
